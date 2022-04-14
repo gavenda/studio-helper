e23b75a21a721a0f9c2ca8646e419986f7ec4d39
Gavenda <gavenda@disroot.org>
2022-04-14 16:17:46 +0800

    Update ani radio uri

--- extension/aniradio/src/main/kotlin/bogus/extension/aniradio/Constants.kt
+++ extension/aniradio/src/main/kotlin/bogus/extension/aniradio/Constants.kt
@@ -3,4 +3,4 @@ package bogus.extension.aniradio
 const val FRAME_BUFFER_SIZE = 1024
 const val EXTENSION_NAME = "aniradio"
 const val TRANSLATIONS_BUNDLE = "aniradio"
-const val ANIME_RADIO_URI = "https://listen.moe/opus"
+const val ANIME_RADIO_URI = "https://listen.moe/m3u8/jpop.m3u"
