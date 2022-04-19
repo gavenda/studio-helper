9b99e7dd967a97b6069108263d1bd7efd19147da
Gavenda <gavenda@disroot.org>
2022-04-19 16:33:08 +0800

    Fix lavakord deps

--- extension/music/build.gradle.kts
+++ extension/music/build.gradle.kts
@@ -6,15 +6,15 @@ version = "1.0"
 
 dependencies {
     implementation(project(":common"))
-    implementation("dev.schlaubi.lavakord:kord:3.5.1")
+    implementation("dev.schlaubi.lavakord:kord:3.6.0")
     implementation("de.sonallux.spotify:spotify-web-api-java:2.4.0")
 
     // DB
     implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.flywaydb:flyway-core:8.4.3")
+    implementation("org.flywaydb:flyway-core:8.5.8")
     implementation("org.ktorm:ktorm-core:3.4.1")
     implementation("org.ktorm:ktorm-support-postgresql:3.4.1")
-    runtimeOnly("org.postgresql:postgresql:42.3.1")
+    runtimeOnly("org.postgresql:postgresql:42.3.4")
 }
 
 tasks {
