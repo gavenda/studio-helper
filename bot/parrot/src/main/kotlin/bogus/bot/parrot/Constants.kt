adf37cc3e9ce769b2a4442b9fcd117cba0457c81
Gavenda <gavenda@disroot.org>
2022-04-29 19:13:40 +0800

    Add new voice lines

--- bot/parrot/src/main/kotlin/bogus/bot/parrot/Constants.kt
+++ bot/parrot/src/main/kotlin/bogus/bot/parrot/Constants.kt
@@ -1,7 +1,13 @@
 package bogus.bot.parrot
 
 val AUDIO_FILES = mapOf(
+    "Jahusa: Gusto kag chula bro?" to "jahusa_chula_bro.mp3",
+    "Jahusa: We're down bad for Neil" to "jahusa_down_bad_neil.mp3",
+    "Jelly: *gremlin noises*" to "jelly_gremlin.mp3",
+    "Jelly: I am good" to "jelly_i_am_good.mp3",
+    "Jelly: Pest Pretty" to "jelly_pest_pretty.mp3",
     "Mang2x: Sunda Imo Heart" to "mang_sunda_imo_heart.mp3",
+    "Mang2x: Nanay Doggy" to "mang_nanay_doggy.mp3",
     "Neil: Dog" to "neil_dog.mp3",
     "Neil: Kanusa Iyoton" to "neil_kanusa_iyoton.mp3",
     "Neil: Kaon Itlog" to "neil_kaon_itlog.mp3",
